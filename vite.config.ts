65a7780352f6f69d7cc8b2a8009801b166632dab
a <a@a.com>
2025-06-12 12:01:44 -0300

    configuracoes de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dia-dos-namorados-main",
   plugins: [react()],
+  base: "/dia-dos-namorados-main",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
